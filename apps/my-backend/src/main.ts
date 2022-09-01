e12c03a0e02224d3963cc147e7293d970ea0709f
beauwilliams <beau.williams@icloud.com>
2022-09-01 16:02:43 +1000

    feat: add graphql config to nest, replace express with fastify

--- apps/my-backend/src/main.ts
+++ apps/my-backend/src/main.ts
@@ -5,11 +5,12 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
