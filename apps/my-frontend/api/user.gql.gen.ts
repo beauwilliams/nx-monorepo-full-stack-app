83bf86ba206b71c7d2e81535385a94abbe61efba
beauwilliams <beau.williams@icloud.com>
2022-09-03 09:34:26 +1000

    feat: successfully auto generated client side gql hooks with gen:gql

---
+++ apps/my-frontend/api/user.gql.gen.ts
@@ -0,0 +1,25 @@
+import * as Types from '@full-stack/my-client/generated/graphql-types';
+
+import { gql } from 'urql';
+import * as Urql from 'urql';
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export type GetUserQueryVariables = Types.Exact<{
+  args: Types.UserWhereUniqueInput;
+}>;
+
+
+export type GetUserQuery = { __typename?: 'Query', user: { __typename?: 'User', name?: string | null, email: string } };
+
+
+export const GetUserDocument = gql`
+    query GetUser($args: UserWhereUniqueInput!) {
+  user(where: $args) {
+    name
+    email
+  }
+}
+    `;
+
+export function useGetUserQuery(options: Omit<Urql.UseQueryArgs<GetUserQueryVariables>, 'query'>) {
+  return Urql.useQuery<GetUserQuery, GetUserQueryVariables>({ query: GetUserDocument, ...options });
+};
