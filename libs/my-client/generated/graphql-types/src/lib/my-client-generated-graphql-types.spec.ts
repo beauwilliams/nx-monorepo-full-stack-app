39a76c6c0c41e77f40261caed02a409821e5c494
beauwilliams <beau.williams@icloud.com>
2022-09-03 18:22:14 +1000

    fix: refactor gen gql types location, remove .sped.ts unit tests as we only have types now in this library, no logic

--- libs/my-client/generated/graphql-types/src/lib/my-client-generated-graphql-types.spec.ts
+++
@@ -1,9 +0,0 @@
-import { myClientGeneratedGraphqlTypes } from './my-client-generated-graphql-types';
-
-describe('myClientGeneratedGraphqlTypes', () => {
-  it('should work', () => {
-    expect(myClientGeneratedGraphqlTypes()).toEqual(
-      'my-client-generated-graphql-types'
-    );
-  });
-});
